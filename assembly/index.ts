712b4086c1a43450566e5c59196c13885004e2e3
Tommaso Girotto <tommaso.girotto91@gmail.com>
2025-09-05 20:17:00 +0200

    no change

--- assembly/index.ts
+++ assembly/index.ts
@@ -6,11 +6,11 @@ import { JSON } from "json-as/assembly";
 class Input { a!: i32; b!: i32; }
 
 @json
-class Output { sum!: i32; }
+class Output { result!: i32; }
 
 export function run(input: string): string {
   // const res = JSON.parse(input);
   const obj = JSON.parse<Input>(input); // throws if invalid
-  const sum = obj.a + obj.b;
-  return JSON.stringify<Output>({ sum });
+  const result = obj.a + obj.b;
+  return JSON.stringify<Output>({ result });
 }
